cadeed708b8bd9ce37a98b9f172c31ebbf7c4e8a
leonardoolia <leonardooliaa@gmail.com>
2024-02-21 15:24:16 +0100

    created function to get random password

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+// Recupero il valore dal form
+$password_length = $_GET['length'] ?? '';
+
+// Funzione per generare password casuale
+function get_random_password ($password_length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    $random_string ='';
+
+    for($i = 0; $i < $password_length; $i++){
+        $random_string .= $characters[rand(0, mb_strlen($characters) -1)];
+    }
+
+    return $random_string;
+};
+
+$random_password=get_random_password($password_length);
+
+var_dump($random_password);
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -22,16 +46,16 @@
     </header>
     <main>
         <div class="container">
-            <form>
+            <form method="GET" action="">
 
                 <div class="mb-3 d-flex justify-content-between">
-                    <label for="password" class="form-label fs-2">Lunghezza password:</label>
-                    <input type="text" class="form-control w-50" id="password" name="">
+                    <label for="password" class="form-label fs-2">Lunghezza password (minimo 5 caratteri):</label>
+                    <input type="number" class="form-control w-50" id="password" name="length" min="5">
                 </div>
 
                 <div class="buttons mt-5">
                     <button type="submit" class="btn btn-primary">Genera</button>
-                    <button type="submit" class="btn btn-secondary">Annulla</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
                 </div>
 
             </form>
